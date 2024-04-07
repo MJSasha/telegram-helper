3e94581c412ad1ed005d7807b73ad953eb53b591
Sasha Ushankov <sasha.ushankov@gmail.com>
2024-04-07 23:04:12 +0300

    Added buttons

--- TelegramHelper/Controllers/BaseMessagesController.cs
+++ TelegramHelper/Controllers/BaseMessagesController.cs
@@ -1,4 +1,7 @@
 using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using TelegramHelper.Definitions;
 using TgBotLib.Core;
 using TgBotLib.Core.Base;
 
@@ -6,9 +9,45 @@ namespace TelegramHelper.Controllers;
 
 public class BaseMessagesController : BotController
 {
+    private readonly IInlineButtonsGenerationService _buttonsGenerationService;
+
+    public BaseMessagesController(IInlineButtonsGenerationService buttonsGenerationService)
+    {
+        _buttonsGenerationService = buttonsGenerationService;
+    }
+
     [Message("/start")]
     public Task Start()
     {
-        return Client.SendTextMessageAsync(BotContext.Update.GetChatId(), "Hello world!");
+        return Client.SendTextMessageAsync(Update.GetChatId(),
+            Messages.Base.StartText,
+            replyMarkup: GetButtons(),
+            parseMode: ParseMode.Markdown);
+    }
+
+    [Callback("buttonClick")]
+    public Task ButtonClicket()
+    {
+        return Client.SendTextMessageAsync(Update.GetChatId(),
+            Messages.Base.ButtonClicked,
+            replyMarkup: GetButtons(),
+            parseMode: ParseMode.Markdown);
+    }
+
+    [UnknownUpdate]
+    [UnknownMessage]
+    public Task UnknownMessage()
+    {
+        return Client.SendTextMessageAsync(Update.GetChatId(),
+            Messages.Base.UnknownMessage,
+            replyMarkup: GetButtons(),
+            parseMode: ParseMode.Markdown);
+    }
+
+    private IReplyMarkup GetButtons()
+    {
+        _buttonsGenerationService.SetInlineButtons(("Кнопочка", "buttonClick"), ("Кнопочка", "buttonClick"));
+        _buttonsGenerationService.SetInlineButtons(("Ух.. Кнопочка", "buttonClick"));
+        return _buttonsGenerationService.GetButtons();
     }
 }
