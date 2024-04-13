696dde4f9429eac682253e2435eb051aa6633155
Sasha Ushankov <sasha.ushankov@gmail.com>
2024-04-13 12:43:02 +0300

    Updated user info model

--- TelegramHelper.Domain/Entities/User.cs
+++ TelegramHelper.Domain/Entities/User.cs
@@ -5,6 +5,9 @@ namespace TelegramHelper.Domain.Entities;
 public class User
 {
     public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string Username { get; set; }
+    public string LanguageCode { get; set; }
     public long ChatId { get; set; }
     public UserRole Role { get; set; }
 }
